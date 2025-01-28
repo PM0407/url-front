8a6100978359ae09913a696cf99530a25db9328d
PM0407 <prasannamurthy2006@gmail.com>
2025-01-28 17:07:42 +0530

    First

--- src/components/ShortenerForm.js
+++ src/components/ShortenerForm.js
@@ -14,7 +14,7 @@ const ShortenerForm = () => {
     setLoading(true);
 
     try {
-      const response = await axios.post("https://your-backend-url/api/shorten", {
+      const response = await axios.post("https://url-production-42af.up.railway.app/api/shorten", {
         originalUrl,
       });
       setShortUrl(response.data.shortUrl);
